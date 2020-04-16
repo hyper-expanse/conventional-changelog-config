5b5f95db38bbe837b9e358f49162d3701a48308f
Hutson Betts <hutson@hyper-expanse.net>
2020-04-15 23:11:56 -0500

    style(src): fix lint errors

--- src/index.integration.spec.js
+++ src/index.integration.spec.js
@@ -2,71 +2,71 @@
 
 /* eslint-disable no-unused-expressions */
 
-const chai = require(`chai`);
-const chaiAsPromised = require(`chai-as-promised`);
-const { describe, it } = require(`mocha`);
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { describe, it } = require('mocha');
 
-const conventionalChangelogConfig = require(`../`);
+const conventionalChangelogConfig = require('../');
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
-describe(`@hutson/conventional-changelog-config`, () => {
-  it(`is an object`, () => {
-    expect(conventionalChangelogConfig).to.be.a(`promise`);
+describe('@hutson/conventional-changelog-config', () => {
+  it('is an object', () => {
+    expect(conventionalChangelogConfig).to.be.a('promise');
   });
 
-  it(`should have a whatBump function on 'conventional-recommended-bump`, () => {
+  it('should have a whatBump function on \'conventional-recommended-bump', () => {
     return expect(conventionalChangelogConfig).to.be.fulfilled
       .then(configuration => {
-        [`parserOpts`, `writerOpts`, `conventionalChangelog`, `recommendedBumpOpts.whatBump`].forEach(property => {
+        ['parserOpts', 'writerOpts', 'conventionalChangelog', 'recommendedBumpOpts.whatBump'].forEach(property => {
           expect(configuration).to.have.nested.property(property);
         });
       });
   });
 
-  it(`should return expected results`, () => {
+  it('should return expected results', () => {
     const commits = [{
       notes: [],
-      type: `chore`
+      type: 'chore'
     }, {
       notes: [],
-      type: `fix`
+      type: 'fix'
     }, {
       notes: [],
-      type: `feat`
+      type: 'feat'
     }, {
       notes: [{
-        title: `BREAKING CHANGE`,
-        text: `Something in the API has changed.`
+        title: 'BREAKING CHANGE',
+        text: 'Something in the API has changed.'
       }],
-      type: `fix`
+      type: 'fix'
     }, {
       notes: [{
-        title: `BREAKING CHANGE`,
-        text: `Something in the API has changed.`
+        title: 'BREAKING CHANGE',
+        text: 'Something in the API has changed.'
       }, {
-        title: `BREAKING CHANGE`,
-        text: `Something else in the API has changed.`
+        title: 'BREAKING CHANGE',
+        text: 'Something else in the API has changed.'
       }],
-      type: `feat`
+      type: 'feat'
     }];
 
     const results = [{
       level: null,
-      reason: `There are 0 BREAKING CHANGES and 0 features`
+      reason: 'There are 0 BREAKING CHANGES and 0 features'
     }, {
       level: 2,
-      reason: `There are 0 BREAKING CHANGES and 0 features`
+      reason: 'There are 0 BREAKING CHANGES and 0 features'
     }, {
       level: 1,
-      reason: `There are 0 BREAKING CHANGES and 1 feature`
+      reason: 'There are 0 BREAKING CHANGES and 1 feature'
     }, {
       level: 0,
-      reason: `There is 1 BREAKING CHANGE and 0 features`
+      reason: 'There is 1 BREAKING CHANGE and 0 features'
     }, {
       level: 0,
-      reason: `There are 2 BREAKING CHANGES and 1 feature`
+      reason: 'There are 2 BREAKING CHANGES and 1 feature'
     }];
 
     return expect(conventionalChangelogConfig).to.be.fulfilled
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const conventionalChangelog = require(`conventional-changelog-angular/conventional-changelog`);
-const parserOpts = require(`conventional-changelog-angular/parser-opts`);
-const recommendedBumpOpts = require(`./conventional-recommended-bump`);
-const writerOpts = require(`conventional-changelog-angular/writer-opts`);
+const conventionalChangelog = require('conventional-changelog-angular/conventional-changelog');
+const parserOpts = require('conventional-changelog-angular/parser-opts');
+const recommendedBumpOpts = require('./conventional-recommended-bump');
+const writerOpts = require('conventional-changelog-angular/writer-opts');
 
 module.exports = Promise
   .all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
